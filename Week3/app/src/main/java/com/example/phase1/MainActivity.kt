c2ea8ee464cd9d97f146aec821ff08bc0f832083
jason white <jasonwhitesg@gmail.com>
2023-10-03 12:33:52 -0600

    updating

--- Week3/app/src/main/java/com/example/phase1/MainActivity.kt
+++ Week3/app/src/main/java/com/example/phase1/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.phase1
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
     private val viewModel: SimpleView by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("MainActivity", "Before setting the theme.")
+        setTheme(R.style.Theme_Phase1)
+        Log.d("MainActivity", "After setting the theme.")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
 }
