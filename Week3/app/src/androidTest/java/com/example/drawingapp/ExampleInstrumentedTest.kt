1be20117d7be3557b5425aa1742af2a97179e53c
Keegan Dohm <keegan.dohm@hsc.utah.edu>
2023-10-05 23:09:12 -0600

    updated exampleinstrumented test to appropriate appname

--- Week3/app/src/androidTest/java/com/example/drawingapp/ExampleInstrumentedTest.kt
+++ Week3/app/src/androidTest/java/com/example/drawingapp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.phase1", appContext.packageName)
+        assertEquals("com.example.drawingapp", appContext.packageName)
     }
 }
