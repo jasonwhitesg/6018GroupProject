886117e1b27c3009732ca9fecebb0cc594b51b0d
ricardo2892 <ricardo.sanchez.2830@gmail.com>
2023-10-05 20:02:03 -0600

    Adding Tests

--- Week3/app/src/androidTest/java/com/example/drawingapp/LandingFragmentTest.kt
+++ Week3/app/src/androidTest/java/com/example/drawingapp/LandingFragmentTest.kt
@@ -2,33 +2,59 @@ package com.example.drawingapp
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.core.app.ActivityScenario
-import androidx.test.espresso.matcher.ViewMatchers.hasDescendant
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
-import com.example.drawingapp.R
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-
-
 
 @RunWith(AndroidJUnit4::class)
 @MediumTest
 class LandingFragmentTest {
 
+    private var activityScenario: ActivityScenario<MainActivity>? = null
+
+    @Before
+    fun setUp() {
+        // Launch the main activity
+        activityScenario = ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    @After
+    fun tearDown() {
+        // Close the activity after each test
+        activityScenario?.close()
+    }
+
     @Test
     fun testNavigationToLetsDraw() {
-        // Launch the main activity
-        ActivityScenario.launch(MainActivity::class.java)
-
         // Perform a click action on the button
         onView(withId(R.id.button)).perform(click())
 
-        // Check that the LetsDraw fragment is displayed
-        onView(withId(R.id.fragmentContainerView))
-            .check(matches(hasDescendant(withId(R.id.customView))))
+        // Check if the DrawingFragment is displayed
+        onView(withId(R.id.drawingFragment)).check(matches(isDisplayed()))
+
+        // Check if specific views or buttons in DrawingFragment are displayed
+        onView(withId(R.id.button3)).check(matches(isDisplayed())) // Color picker button
+        onView(withId(R.id.button4)).check(matches(isDisplayed())) // Save drawing button
+        onView(withId(R.id.button6)).check(matches(isDisplayed())) // Load Drawing button
+        onView(withId(R.id.button7)).check(matches(isDisplayed())) // Clear Drawing button
+        onView(withId(R.id.sizeSlider)).check(matches(isDisplayed())) // Brush Size Slider
     }
+
+    @Test
+    fun testNavigationToLoadDrawing() {
+        // Perform a click action on the "Load Drawing" button
+        onView(withId(R.id.button2)).perform(click())
+
+        // Check if the LoadDrawingFragment is displayed
+        //onView(withId(R.id.LoadDrawingFragment)).check(matches(isDisplayed()))
+    }
+
 }
 
